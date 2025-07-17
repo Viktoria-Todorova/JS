8b44f66717481dee37c62a89e6de9129b8becba2
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-18 00:01:02 +0300

    5

--- 5-DOM Introduction/Exercises/06. Format the Text/format-the-text.js
+++ 5-DOM Introduction/Exercises/06. Format the Text/format-the-text.js
@@ -1,3 +1,22 @@
 function solve() {
-  // TODO
+    const textArealEl = document.getElementById('input');
+    const outputEl = document.getElementById('output');
+
+    const text = textArealEl.value;
+    const sentences = text.split('.').filter(sentence => sentence.length > 0);
+
+    let currentParagraph = '';
+    for (let i=0; i< sentences.length;i++){
+      const sentece =sentences[i].trim();
+      currentParagraph += sentece + '.';
+      if ((i+1) % 3 === 0 || i === sentences.length -1){
+        const parargEl = `<p>${currentParagraph}</p>`;
+        outputEl.innerHTML += parargEl;
+        currentParagraph = '';
+      }
+    }
+
+
+    
+
 }
--- 5-DOM Introduction/Exercises/09. Number Convertor/number-convertor.js
+++ 5-DOM Introduction/Exercises/09. Number Convertor/number-convertor.js
@@ -1,3 +1,14 @@
 function solve() {
-    // TODO
+    const numberInputEl = document.getElementById('input');
+    const slecTmENUfROMeL = document.getElementById('selectMenuTo');
+    const resultInputEl =document.getElementById('result');
+
+    const num =Number(numberInputEl.value.trim());
+    const convertionUnit = slecTmENUfROMeL.value;
+
+    if (convertionUnit === 'binary'){
+        resultInputEl.value += num.toString(2);
+    }else if(convertionUnit === 'hexadecimal'){
+        resultInputEl.value += num.toString(16).toUpperCase();
+    }
 }
