b148cb82f1a028661248db2f95e55c86b68e8910
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-17 14:57:20 +0300

    5

--- 5-DOM Introduction/Exercises/01. Subtraction/subtraction.js
+++ 5-DOM Introduction/Exercises/01. Subtraction/subtraction.js
@@ -1,3 +1,13 @@
 function subtract() {
-    //TODO
+    const firstNum = document.getElementById('firstNumber');
+    const secondNum= document.getElementById('secondNumber');
+    const resultPlace = document.getElementById('result');
+
+    let num1 = Number(firstNum.value);
+    let num2 = Number(secondNum.value);
+
+    let result = num1 -num2;
+    resultPlace.textContent = result;
+    
+
 }
--- 5-DOM Introduction/Exercises/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ 5-DOM Introduction/Exercises/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+    let textToConvert = document.getElementById('text').value;
+    let typeOfText = document.getElementById('naming-convention').value;
+    let finalText = '';
+
+    let textToLowercase = textToConvert.toLowerCase();
+    
+    if(typeOfText === 'Camel Case'){
+
+    }else if(typeOfText === 'Pascal Case'){
+
+    }else{
+      finalText += 'Eroor!'
+    }
+    
 }
