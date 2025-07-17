8767cd7cc149df21614c5c76866872c88020d57c
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-17 22:17:49 +0300

    5

--- 5-DOM Introduction/Exercises/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ 5-DOM Introduction/Exercises/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,16 +1,19 @@
 function solve() {
-    let textToConvert = document.getElementById('text').value;
-    let typeOfText = document.getElementById('naming-convention').value;
-    let finalText = '';
-
-    let textToLowercase = textToConvert.toLowerCase();
+    let textInput = document.getElementById('text');
+    let typeOfTextInput = document.getElementById('naming-convention');
+    const resultSpanEl = document.getElementById('result');
+   
+    const texttoConvert = textInput.value.trim().toLowerCase(); //to remove spaces
+    const namigConvention = typeOfTextInput.value.trim();
+    const words = texttoConvert.split(' ');
     
-    if(typeOfText === 'Camel Case'){
 
-    }else if(typeOfText === 'Pascal Case'){
-
+    if(namigConvention === 'Camel Case'){
+        resultSpanEl.textContent = words[0]+ words.slice(1).map(word => word[0].toUpperCase() + word.slice(1)).join('');
+    }else if(namigConvention === 'Pascal Case'){
+        resultSpanEl.textContent = words.map(word => word[0].toUpperCase() + word.slice(1)).join('');
     }else{
-      finalText += 'Eroor!'
+        resultSpanEl.textContent = 'Error!'
     }
     
-}
+} 
--- 5-DOM Introduction/Exercises/03. Accordion/accordion.js
+++ 5-DOM Introduction/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    //TODO
+    const buttonEl = document.querySelector('.button');
+    const extraDivEl = document.getElementById('extra');
+
+    if (buttonEl.textContent === 'More'){
+        extraDivEl.style.display = 'block';
+        buttonEl.textContent = 'Less';
+    }else {
+        extraDivEl.style.display = 'none';
+        buttonEl.textContent = 'More';
+    }
 }
