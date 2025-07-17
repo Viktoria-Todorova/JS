c6b5862a609bdd1a97d51b10ffef06b7db670987
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-17 23:27:56 +0300

    5

--- 5-DOM Introduction/Exercises/04. Search in List/search-in-list.js
+++ 5-DOM Introduction/Exercises/04. Search in List/search-in-list.js
@@ -1,3 +1,21 @@
 function solve() {
-   // TODO
+   const allLiEl = document.querySelectorAll('#towns li');
+   const searshInputEl = document.getElementById('searchText');
+   const resultDivEl =document.getElementById('result');
+
+   const searsh = searshInputEl.value.trim();
+   let matches = 0;
+
+   allLiEl.forEach(liEl => {
+      if(liEl.textContent.includes(searsh)){
+         liEl.style.fontWeight='bold';
+         liEl.style.textDecoration ='underline';
+         matches++;
+      }else{
+         liEl.style.fontWeight='normal';
+         liEl.style.textDecoration ='none';
+      }
+   })
+
+   resultDivEl.textContent = `${matches} matches found`
 }
--- 5-DOM Introduction/Exercises/05. Table - Search Engine/table-search-engine.js
+++ 5-DOM Introduction/Exercises/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,26 @@
 function solve() {
-   //TODO
+   const allTdEl = document.querySelectorAll('tbody td');
+   const allTrEl = document.querySelectorAll('tbody tr');
+
+   const searshInputEl = document.getElementById('searchField');
+
+   allTrEl.forEach(trEl => {
+      trEl.classList.remove('select')
+   })
+   
+
+   const search = searshInputEl.value.trim().toLowerCase();
+
+   if(!search){
+      return;
+   }
+
+
+   allTdEl.forEach(tdEl =>{
+      if (tdEl.textContent.toLowerCase().includes(search)){
+         const trEl =tdEl.parentElement;
+         trEl.classList.add('select');
+      }
+   }
+   )
 }
