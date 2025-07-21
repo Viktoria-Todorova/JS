f3aa2319c11e81745f4e121784abb38cad6afec8
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-21 15:40:37 +0300

    5

--- 5-DOM Introduction/Exercises/08. Generate Report/generate-report.js
+++ 5-DOM Introduction/Exercises/08. Generate Report/generate-report.js
@@ -1,3 +1,36 @@
 function solve() {
-    //TODO
+    const allaCheckedeL = document.querySelectorAll('input[type="checkbox"]');
+    const allTrEL =document.querySelectorAll('tbody tr');
+
+    const outputTextArea = document.getElementById('output');
+
+    const checkCols =[];
+    allaCheckedeL.forEach((checkboxEl,idx) =>{
+        if (checkboxEl.checked){
+            checkCols.push({
+                index:idx,
+                name:checkboxEl.name
+            });
+        }
+    });
+    
+    let report =[];
+
+    allTrEL.forEach(trEl =>{
+        const tdEls =trEl.children;
+        let rowData = {};
+        checkCols.forEach(col => {
+            const propName = col.name;
+            const propValueIndex =col.index;
+            const propValue =tdEls[propValueIndex].textContent;
+            rowData[propName] = propValue;
+        })
+        report.push(rowData);
+    }
+
+    )
+
+    outputTextArea.value = JSON.stringify(report);
+    
+     
 }
