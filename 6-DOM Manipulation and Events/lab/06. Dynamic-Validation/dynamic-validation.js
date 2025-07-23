f848407be8f03d958d56c8a4f77d7f191fc82d2f
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-23 10:09:14 +0300

    6

--- 6-DOM Manipulation and Events/lab/06. Dynamic-Validation/dynamic-validation.js
+++ 6-DOM Manipulation and Events/lab/06. Dynamic-Validation/dynamic-validation.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const inputValidator = document.getElementById('email');
+    
+    let pattern = /[a-z]+@[a-z]+\./g;
+    
+    inputValidator.addEventListener('change', handleInputChange);
+
+    function handleInputChange(){
+        const emailToCheck = inputValidator.value.trim();
+
+        if (!pattern.test(emailToCheck)){
+            inputValidator.classList.add('error'); //add class error to the field
+
+        }else{
+            inputValidator.classList.remove('error');
+        }
+    }
+
+
 }
