0e9ffbe97f40aa97f31b908a717180de14cb8d11
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-23 09:03:20 +0300

    6

--- 6-DOM Manipulation and Events/lab/03. Add-Delete/add-delete.js
+++ 6-DOM Manipulation and Events/lab/03. Add-Delete/add-delete.js
@@ -1,3 +1,23 @@
 function addItem() {
-    // TODO
+    const inputTextEl = document.getElementById('newItemText');
+    const listOfItems = document.getElementById('items');
+    const newTextItem = inputTextEl.value.trim();
+
+    const newLiEl = document.createElement('li');
+
+    newLiEl.textContent = newTextItem;
+    const deleteAel = document.createElement('a');
+    deleteAel.textContent = '[Delete]';
+    deleteAel.href = '#';
+    deleteAel.addEventListener('click',handleDelete);
+
+    function handleDelete(){
+        newLiEl.remove();
+    }
+    newLiEl.appendChild(deleteAel);
+
+    listOfItems.appendChild(newLiEl);
+    
+    
+    
 }
