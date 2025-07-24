2e5649d16fb67e120588f800935a765cd786db5a
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-24 12:11:05 +0300

    6

--- 6-DOM Manipulation and Events/Exercises/02. Time-Converter/time-converter.js
+++ 6-DOM Manipulation and Events/Exercises/02. Time-Converter/time-converter.js
@@ -1,5 +1,44 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const allFromEl = document.querySelectorAll('form');
+
+    const daysInputEl = document.getElementById('days-input');
+    const hoursInputEl = document.getElementById('hours-input');
+    const minutesInputEl = document.getElementById('minutes-input');
+    const seconsdInputEl = document.getElementById('seconds-input');
+
+    allFromEl.forEach(formEl => formEl.addEventListener('submit', handleFormSumbit));
+
+    function handleFormSumbit(e) {
+        e.preventDefault();
+
+        const currentFormEl = e.target;
+        const currentInputEl = currentFormEl.querySelector('input[type="number"]');
+        const originalValue = Number(currentInputEl.value.trim());
+
+        if (currentFormEl.id === 'days') {
+            hoursInputEl.value = (originalValue * 24).toFixed(2);
+            minutesInputEl.value = (originalValue * 1440).toFixed(2);
+            seconsdInputEl.value = (originalValue * 86400).toFixed(2);
+
+        } else if (currentFormEl.id === 'hours') {
+            const days = originalValue / 24;
+            daysInputEl.value = days.toFixed(2);
+            minutesInputEl.value = (days * 1440).toFixed(2);
+            seconsdInputEl.value = (days * 86400).toFixed(2);
+
+        } else if (currentFormEl.id === 'minutes') {
+            const days = originalValue / 1440;
+            daysInputEl.value = days.toFixed(2);
+            hoursInputEl.value = (days * 24).toFixed(2);
+            seconsdInputEl.value = (days * 86400).toFixed(2);
+
+        } else if (currentFormEl.id === 'seconds') {
+            const days = originalValue / 86400;
+            daysInputEl.value = days.toFixed(2);
+            hoursInputEl.value = (days * 24).toFixed(2);
+            minutesInputEl.value = (days * 1440).toFixed(2);
+        }
+    }
 }
--- 6-DOM Manipulation and Events/Exercises/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ 6-DOM Manipulation and Events/Exercises/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,45 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const sendetrTextEl = document.querySelector('#encode textarea');
+    const receiverTextEl = document.querySelector('#decode textarea');
+
+    const encodeBtnEl = document.querySelector('#encode button');
+    const decodeBtnEl = document.querySelector('#decode button');
+
+    encodeBtnEl.addEventListener('click',handleEncodeMsg);
+    decodeBtnEl.addEventListener('click', handleDecodeMsg);
+    function handleEncodeMsg(e){
+        e.preventDefault();
+        const originalMsg = sendetrTextEl.value.trim();
+        let encodedmsg = '';
+
+        for(const ch of originalMsg){
+            const originalaSCII = ch.charCodeAt();
+            const newAscii = originalaSCII +1;
+            const newChar = String.fromCharCode(newAscii);
+            encodedmsg += newChar;
+        }
+
+        sendetrTextEl.value = '';
+        receiverTextEl.value = encodedmsg;
+    }
+
+    function handleDecodeMsg(e){
+        e.preventDefault();
+
+        const encodedMsg = receiverTextEl.value.trim();
+        let decodedmsg = '';
+
+        for(const ch of encodedMsg){
+            const originalaSCII = ch.charCodeAt();
+            const newAscii = originalaSCII - 1;
+            const newChar = String.fromCharCode(newAscii);
+            decodedmsg += newChar;
+        }
+
+        
+        receiverTextEl.value = decodedmsg;
+    }
+
 }
