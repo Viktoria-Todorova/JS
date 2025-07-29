fd13af49f0165226b6a45c886ad9f299470e502f
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-07-29 10:01:46 +0300

    7

---
+++ 7-Asynchronous Programming/lab/04. Github-Commits/app.js
@@ -0,0 +1,31 @@
+function loadCommits() {
+    const usernameinputEl=document.querySelector('#username');
+    const repoURL = document.querySelector('#repos');
+    repoURL.innerHTML= '';
+    const username = usernameinputEl.value.trim();
+
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(res => {
+            if (!res.ok){
+                throw new Error(res.status);
+            }
+            return res.json();
+        } )
+        .then(allrepos=> {
+            allrepos.forEach(repoObj => {
+                const liEl = document.createElement('li');
+                const aEl = document.createElement('a');
+                aEl.href = repoObj.html_url;
+                aEl.textContent =repoObj.full_name;
+
+                liEl.appendChild(aEl);
+                repoURL.appendChild(liEl);
+            })
+        })
+        .catch(err=> {
+            const liEl = document.createElement('li');
+            liEl.textContent = `${err} (Not Found)`;
+            repoURL.appendChild(liEl);
+            
+        })
+}
