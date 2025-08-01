a073519beeba386a3e7e116826f32431a12f514d
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-08-01 12:58:38 +0300

    8

--- 8-HTTP and AJAX/Exercises/01.Blog/app.js
+++ 8-HTTP and AJAX/Exercises/01.Blog/app.js
@@ -1,5 +1,61 @@
+const loadBtnEl = document.querySelector('#btnLoadPosts');
+const postselectEl = document.querySelector('#posts');
+const vieBtnEl = document.querySelector('#btnViewPost');
+const titleH1El = document.querySelector('#post-title');
+const BodyEl = document.querySelector('#post-body');
+const commentUlEl = document.querySelector('#post-comments');
+
+
 function attachEvents() {
-    console.log('TODO...');
+    loadBtnEl.addEventListener('click', handleLoadPost);
+    vieBtnEl.addEventListener('click',handleDisplayPost);
+
+    async function handleLoadPost(){
+        const postRes =await fetch('http://localhost:3030/jsonstore/blog/posts');
+        const postData = await postRes.json();
+        const postArr = Object.values(postData);
+
+        postArr.forEach(postob => {
+            const optionalEl = document.createElement('option');
+            optionalEl.value = postob.id;
+            optionalEl.textContent= postob.title ;
+
+            postselectEl.appendChild(optionalEl);
+        })
+        
+
+    }
+
+    async function handleDisplayPost(){
+        const selectPostId = postselectEl.value;
+
+        const postRes =await fetch('http://localhost:3030/jsonstore/blog/posts');
+        const postData = await postRes.json();
+
+        const postArr = Object.values(postData);
+        const selectedPostObj = postArr.find(p => p.id === selectPostId);
+        titleH1El.textContent =selectedPostObj.title;
+        BodyEl.textContent =selectedPostObj.body;   
+
+        const commentRes = await fetch('http://localhost:3030/jsonstore/blog/comments');
+        const commentsData= await commentRes.json();
+        const commentsArr = Object.values(commentsData);
+        const filteredComments = commentsArr.filter(c => c.postId ===selectPostId);
+
+        commentUlEl.innerHTML = '';
+        
+        filteredComments.forEach(coomentobj => {
+            const lisEl = document.createElement('li');
+            lisEl.textContent =coomentobj.text;
+            commentUlEl.appendChild(lisEl);
+        })
+
+    }
+
+    
+
 }
 
 attachEvents();
+
+//
