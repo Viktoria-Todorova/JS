44087c2e8bea4c1861664e2dedefa7092fd7414d
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-08-05 21:31:18 +0300

    examprep

--- Exam-Prep/22 August 2024/03.Car-Maintenance-Booking_Resources/app.js
+++ Exam-Prep/22 August 2024/03.Car-Maintenance-Booking_Resources/app.js
@@ -1,1 +1,48 @@
-//TODO...
+const loadAppointmentsBtn = document.getElementById('load-appointments');
+loadAppointmentsBtn.addEventListener('click', handleLoadAppoitnemts);
+
+const appoitmentList = document.getElementById('appointments-list');
+
+async function handleLoadAppoitnemts(){
+    appoitmentList.innerHTML = '';
+
+    const createRes = await fetch('http://localhost:3030/jsonstore/appointments/');
+    const ordersData = await createRes.json();
+    const orderArr = Object.values(ordersData);
+    console.log(orderArr);
+
+    orderArr.forEach(orerObj => {
+        const liAppoitmentsEl = document.createElement('li'); //add all here
+        liAppoitmentsEl.classList.add('appointment');
+
+        const h2El =document.createElement('h2');
+        h2El.textContent = orerObj.model;
+
+        const h3Date = document.createElement('h3');
+        h3Date.textContent =orerObj.date;
+
+        const h3Type = document.createElement('h3');
+        h3Type.textContent=orerObj.service;
+
+        liAppoitmentsEl.appendChild(h2El);
+        liAppoitmentsEl.appendChild(h3Date);
+        liAppoitmentsEl.appendChild(h3Type);
+
+        const divElButtons = document.createElement('div');// add buttons here
+        divElButtons.classList.add('buttons-appointment');
+
+        const changeBtn = document.createElement('button');
+        changeBtn.classList.add('change-btn');
+        changeBtn.textContent = 'Change';
+        changeBtn.addEventListener('click', handleChangeBtn);
+
+
+        const deletebtn = 
+        function handleChangeBtn(){
+            console.log(`change btn`);
+            
+        };
+
+    })
+
+}
