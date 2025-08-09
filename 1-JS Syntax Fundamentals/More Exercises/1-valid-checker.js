0ae5e60fabd0481f34119de35cf4989e03c3b503
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-08-09 10:00:04 +0300

    moreexersise

--- 1-JS Syntax Fundamentals/More Exercises/1-valid-checker.js
+++ 1-JS Syntax Fundamentals/More Exercises/1-valid-checker.js
@@ -1,7 +1,31 @@
-function valifityChecker(x1,y1,x2,y2){
+function validityChecker(x1, y1, x2, y2) {
 
-    let distance = Math.sqrt((x2-x2)**2 + (y2-y1)**2)
+    function isValidDistance(a1, b1, a2, b2) {
+        let distance = Math.sqrt((a2 - a1) ** 2 + (b2 - b1) ** 2);
+        return Number.isInteger(distance);
+    }
 
+    // Check 1: (x1, y1) -> (0, 0)
+    if (isValidDistance(x1, y1, 0, 0)) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    // Check 2: (x2, y2) -> (0, 0)
+    if (isValidDistance(x2, y2, 0, 0)) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    // Check 3: (x1, y1) -> (x2, y2)
+    if (isValidDistance(x1, y1, x2, y2)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
 }
 
-valifityChecker(3, 0, 0, 4)
+// Example:
+validityChecker(3, 4, 5, 12);
--- 1-JS Syntax Fundamentals/More Exercises/2-words-uppercase.js
+++ 1-JS Syntax Fundamentals/More Exercises/2-words-uppercase.js
@@ -1,5 +1,18 @@
 function wordsUppercase(inputText){
+    const finalWords = [];
+
+    let words = inputText.split(/[^a-zA-Z]+/);
+
+    for (let word of words) {
+        if(word.length > 0){  // skip empty strings from consecutive punctuation
+            finalWords.push(word.toUpperCase());
+        }
+    }
+    
+    console.log(finalWords.join(', '));
+     
+        
     
 }
 
-wordsUppercase('Hi, how are you?')
+wordsUppercase('Functions in JS can be nested, i.e. hold other functions')
